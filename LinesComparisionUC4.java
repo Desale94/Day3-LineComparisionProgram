f8c2705334aa5074b71b07720f5ab537b0f5e2e2
Desale1 <dkdesale08@gmail.com>
2023-01-23 17:23:23 +0530

    Call the method for lengths

---
+++ LinesComparisionUC4.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Lines;
+
+public class LinesComparisionUC4 {
+    public static void main(String[] args) {
+        double len1 =0,len2=0;
+
+        System.out.println("Welcome to line comparision program!!");
+        LineComparision.length(len1, len2);
+        LineComparision.equal(len1,len2);
+        LineComparision.compare(len1,len2);
+
+    }
+}
